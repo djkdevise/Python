f05db193d77de13258f729c0b726b45681515fd5
Jamith Garcia <jamith.garcia.ext@inetum.com>
2023-09-02 11:48:32 -0500

    Test Template

---
+++ 02_test_template.py
@@ -0,0 +1,30 @@
+#Delcaracion de variables
+name = 'Juana'
+print(name)
+age = '10'
+print(age)
+total = int(age) + 10
+print(total)
+
+
+template = f"Hola mi nombre es {name}, tengo {age} años y en 10 años tendré {total} años"
+print(template)
+
+#Pruebas
+"""
+Pruebas 1
+Input:
+name: Andrea
+age: 22
+
+Output:
+Hola mi nombre es Andreaasas, tengo 22 años y en 10 años tendré 32 años
+
+Pruebas 2
+Input:
+name: Andrea
+age: 22
+
+Output:
+Hola mi nombre es Andreaasas, tengo 22 años y en 10 años tendré 32 años
+"""
