e171b6648d489a29eca683ae9961556fca7225b5
Jamith Garcia <jamith.garcia.ext@inetum.com>
2023-09-02 10:57:54 -0500

    Practica con tipos de datos Booleanos en Python

---
+++ Practicas/06_booleans.py
@@ -0,0 +1,20 @@
+
+#Declaracion de variables
+is_single = True
+
+#Vlidamos el tipo de dato
+print(type(is_single))
+
+#Modificamos el valor de la variable
+is_single = False
+
+#Imprimimos el valor de la variable
+print(is_single)
+
+#Invertimos el valor de la variable con el operador not
+print(not True)
+print(not False)
+
+is_single = not is_single
+
+print(is_single) #Deberia imprimir True
